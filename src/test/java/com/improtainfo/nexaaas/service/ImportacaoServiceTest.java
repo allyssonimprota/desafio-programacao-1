00ce0a8706ee9a21781969ff0a07fcb360d94e57
allysson.improta <allysson.improta@gmail.com>
2019-03-11 00:11:27 -0300

    Iniciando os Testes

---
+++ src/test/java/com/improtainfo/nexaaas/service/ImportacaoServiceTest.java
@@ -0,0 +1,22 @@
+package com.improtainfo.nexaaas.service;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ImportacaoServiceTest {
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+	
+	@Test
+	public void test() {
+		
+		
+		fail("Not yet implemented");
+	}
+
+}
